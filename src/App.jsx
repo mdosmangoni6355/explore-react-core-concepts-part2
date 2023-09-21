f48799aa1ea50979f445b0a7bb3fc1100b9bfaf5
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-09-21 17:01:20 +0600

    Friends.jsx and more added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Counter from './Counter';
+import Friends from './Friends';
 import Team from './Team';
+import Users from './Users';
 
 function App() {
 
@@ -19,6 +21,10 @@ function App() {
   return (
     <>
       <h2>React Core Concepts-2</h2>
+
+      <Friends></Friends>
+
+      <Users></Users>
 
       <Team></Team>
 
